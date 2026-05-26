d08a8b5c9892cb241f9f7e328a84c60412ac232c
edatan9 <esintaneda@gmail.com>
2026-05-26 20:43:21 +0300

    add Constructor tests for BorrowRecord

--- src/test/java/com/example/library/unit/BorrowRecordTest.java
+++ src/test/java/com/example/library/unit/BorrowRecordTest.java
@@ -120,22 +120,22 @@ class BorrowRecordTest {
         @Test
         @DisplayName("should set borrow date to today")
         void shouldSetBorrowDateToToday() {
-            // TODO: Verify that new BorrowRecord sets borrowDate to LocalDate.now()
-            fail("Not implemented yet");
+            BorrowRecord record = new BorrowRecord(createSampleBook(), createSampleMember());
+            assertEquals(LocalDate.now(), record.getBorrowDate());
         }
 
         @Test
         @DisplayName("should set due date to 14 days from today")
         void shouldSetDueDateTo14DaysFromToday() {
-            // TODO: Verify dueDate = borrowDate + STANDARD_BORROW_DAYS
-            fail("Not implemented yet");
+            BorrowRecord record = new BorrowRecord(createSampleBook(), createSampleMember());
+            assertEquals(LocalDate.now().plusDays(BorrowRecord.STANDARD_BORROW_DAYS), record.getDueDate());
         }
 
         @Test
         @DisplayName("should set status to BORROWED")
         void shouldSetStatusToBorrowed() {
-            // TODO: Verify default status is BORROWED
-            fail("Not implemented yet");
+            BorrowRecord record = new BorrowRecord(createSampleBook(), createSampleMember());
+            assertEquals(BorrowStatus.BORROWED, record.getStatus());
         }
     }
 }
