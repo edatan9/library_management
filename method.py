43adb91ec1928b708eae5b5e32613cb0b80bf2b3
ecegunal <152962495+ecegunal@users.noreply.github.com>
2026-05-30 22:07:50 +0300

    Create method.py

---
+++ method.py
@@ -0,0 +1,21 @@
+void shouldReturn409_WhenBorrowLimitExceeded() {
+            
+                
+                Member student = memberRepository.save(new Member("Test Student", "test@test.com", MembershipType.STUDENT));
+                Book book1 = bookRepository.save(new Book("Title 1", "Author 1", "111111", 5,Genre.SCIENCE));
+                Book book2 = bookRepository.save(new Book("Title 2", "Author 2", "222222", 5, Genre.SCIENCE));
+                Book book3 = bookRepository.save(new Book("Title 3", "Author 3", "333333", 5,Genre.SCIENCE));
+
+              
+                restTemplate.postForEntity("/api/borrows", new BorrowRequest(student.getId(), book1.getId()), BorrowResponse.class);
+                restTemplate.postForEntity("/api/borrows", new BorrowRequest(student.getId(), book2.getId()), BorrowResponse.class);
+
+                
+                BorrowRequest limitExceededRequest = new BorrowRequest(student.getId(), book3.getId());
+                ResponseEntity<Map> response = restTemplate.postForEntity("/api/borrows", limitExceededRequest, Map.class);
+
+                
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+                assertThat(response.getBody()).isNotNull();
+                assertThat(response.getBody().get("status")).isEqualTo(409);
+            }
